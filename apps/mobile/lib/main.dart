b6e06fcbec6afea4881d765e9540649b7023f94e
spirmo <153791815+spirmo@users.noreply.github.com>
2026-07-08 17:04:11 +0330

    Update main.dart

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -1,19 +1,7 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-import 'features/auth/presentation/pages/login_page.dart';
+import 'app/app.dart';
 
 void main() {
   runApp(const TwoFunApp());
-}
-
-class TwoFunApp extends StatelessWidget {
-  const TwoFunApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
-    );
-  }
 }
