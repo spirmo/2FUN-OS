48d113bb53c8e28c2475a78d7ccecb4a3ab637a0
spirmo <153791815+spirmo@users.noreply.github.com>
2026-07-10 12:02:44 +0330

    Update app.dart

--- apps/mobile/lib/app/app.dart
+++ apps/mobile/lib/app/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../core/routing/app_router.dart';
 import '../core/theme/app_theme.dart';
-import '../features/auth/presentation/pages/login_page.dart';
+import '../features/language/presentation/pages/splash_page.dart';
 
 class TwoFunApp extends StatelessWidget {
   const TwoFunApp({super.key});
