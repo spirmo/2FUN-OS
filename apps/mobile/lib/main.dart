ff5694f937987cb3dd0394b25bea30d4fc3a8e50
spirmo <153791815+spirmo@users.noreply.github.com>
2026-07-14 12:10:47 +0330

    Update main.dart

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/widgets.dart';
 
 import 'app/app.dart';
+import 'core/database/database_service.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await DatabaseService.instance.database;
+
   runApp(const TwoFunApp());
 }
