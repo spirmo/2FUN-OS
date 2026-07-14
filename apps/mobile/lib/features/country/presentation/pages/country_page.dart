045a42dae1e5cbf0e189f50e67cb3c49e7d0fe22
spirmo <153791815+spirmo@users.noreply.github.com>
2026-07-14 21:20:58 +0330

    Create country_page.dart

---
+++ apps/mobile/lib/features/country/presentation/pages/country_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CountryPage extends StatelessWidget {
+  const CountryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Country Page"),
+      ),
+    );
+  }
+}
