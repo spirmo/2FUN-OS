158dd81252288651016e4eaec93619a60b32ab80
spirmo <153791815+spirmo@users.noreply.github.com>
2026-07-14 21:18:21 +0330

    Update country.dart

--- apps/mobile/lib/core/models/country.dart
+++ apps/mobile/lib/core/models/country.dart
@@ -1,1 +1,39 @@
+class Country {
+  final int id;
+  final String nameFa;
+  final String nameEn;
+  final String nameAr;
+  final String language;
+  final String flag;
 
+  const Country({
+    required this.id,
+    required this.nameFa,
+    required this.nameEn,
+    required this.nameAr,
+    required this.language,
+    required this.flag,
+  });
+
+  factory Country.fromJson(Map<String, dynamic> json) {
+    return Country(
+      id: json["id"],
+      nameFa: json["name_fa"],
+      nameEn: json["name_en"],
+      nameAr: json["name_ar"],
+      language: json["language"],
+      flag: json["flag"],
+    );
+  }
+
+  String name(String lang) {
+    switch (lang) {
+      case "fa":
+        return nameFa;
+      case "ar":
+        return nameAr;
+      default:
+        return nameEn;
+    }
+  }
+}
