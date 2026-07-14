47315e11cc3ae8ea7e772b8317bfec4510787678
spirmo <153791815+spirmo@users.noreply.github.com>
2026-07-15 00:56:58 +0330

    Update app_logo.dart

--- apps/mobile/lib/shared/widgets/app_logo.dart
+++ apps/mobile/lib/shared/widgets/app_logo.dart
@@ -2,20 +2,66 @@ import 'package:flutter/material.dart';
 
 import '../../core/constants/app_assets.dart';
 
+enum AppLogoType {
+  splash,
+  welcome,
+  login,
+  dashboard,
+  appBar,
+  normal,
+}
+
 class AppLogo extends StatelessWidget {
-  final double width;
+  final double? width;
+  final double? height;
+  final AppLogoType type;
 
   const AppLogo({
     super.key,
-    this.width = 180,
+    this.width,
+    this.height,
+    this.type = AppLogoType.normal,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      AppAssets.logo,
-      width: width,
-      fit: BoxFit.contain,
+    double logoWidth;
+
+    switch (type) {
+      case AppLogoType.splash:
+        logoWidth = 220;
+        break;
+
+      case AppLogoType.welcome:
+        logoWidth = 150;
+        break;
+
+      case AppLogoType.login:
+        logoWidth = 140;
+        break;
+
+      case AppLogoType.dashboard:
+        logoWidth = 90;
+        break;
+
+      case AppLogoType.appBar:
+        logoWidth = 42;
+        break;
+
+      case AppLogoType.normal:
+        logoWidth = 180;
+        break;
+    }
+
+    return Hero(
+      tag: "twofun_logo",
+      child: Image.asset(
+        AppAssets.logo,
+        width: width ?? logoWidth,
+        height: height,
+        fit: BoxFit.contain,
+        filterQuality: FilterQuality.high,
+      ),
     );
   }
 }
