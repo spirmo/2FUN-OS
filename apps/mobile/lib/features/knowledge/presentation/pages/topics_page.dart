ec3e1505988ea72a5d09d520cba349db8ad2a256
spirmo <153791815+spirmo@users.noreply.github.com>
2026-07-16 14:28:49 +0330

    Create topics_page.dart

---
+++ apps/mobile/lib/features/knowledge/presentation/pages/topics_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TopicsPage extends StatefulWidget {
+  const TopicsPage({super.key});
+
+  @override
+  State<TopicsPage> createState() => _TopicsPageState();
+}
+
+class _TopicsPageState extends State<TopicsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+    );
+  }
+}
