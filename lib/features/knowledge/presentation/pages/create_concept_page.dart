3c3f0c37683f678f827e3dd3f2eac0a58af3a131
spirmo <153791815+spirmo@users.noreply.github.com>
2026-07-18 15:36:01 +0330

    Update create_concept_page.dart

--- lib/features/knowledge/presentation/pages/create_concept_page.dart
+++ lib/features/knowledge/presentation/pages/create_concept_page.dart
@@ -1,22 +1,88 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/language/language_service.dart';
+
+
 class CreateConceptPage extends StatefulWidget {
+
   final int topicId;
+
 
   const CreateConceptPage({
     super.key,
     required this.topicId,
   });
 
+
   @override
   State<CreateConceptPage> createState() => _CreateConceptPageState();
 }
 
+
+
 class _CreateConceptPageState extends State<CreateConceptPage> {
+
+
+  final LanguageService languageService = LanguageService();
+
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    _loadLanguage();
+  }
+
+
+
+  Future<void> _loadLanguage() async {
+
+    final code = await languageService.getLanguage();
+
+    await languageService.load(code);
+
+
+    if (mounted) {
+
+      setState(() {});
+
+    }
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+
+    return Scaffold(
+
       backgroundColor: Colors.black,
+
+
+      appBar: AppBar(
+
+        backgroundColor: Colors.black,
+
+        title: Text(
+          languageService.text("new_concept"),
+        ),
+      ),
+
+
+
+      body: Center(
+
+        child: Text(
+
+          languageService.text("create_concept"),
+
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
     );
   }
 }
