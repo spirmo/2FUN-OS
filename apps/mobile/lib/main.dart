a70442efddb3f8dbeaf679f382669046c15bb753
spirmo <153791815+spirmo@users.noreply.github.com>
2026-07-21 06:09:07 +0330

    Update main.dart

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter/widgets.dart';
 import 'app/app.dart';
 import 'core/database/database_service.dart';
 
-const bool isGovernanceBuild = false;
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
