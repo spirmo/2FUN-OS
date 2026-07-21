3c6b773ff0e8b051e574c0415a4d3811f3c2e767
spirmo <153791815+spirmo@users.noreply.github.com>
2026-07-21 18:55:30 +0330

    Create governance_api_service.dart

---
+++ apps/mobile/lib/features/governance/data/governance_api_service.dart
@@ -0,0 +1,19 @@
+import '../../../../../engines/governance/core/governance_controller.dart';
+
+class GovernanceApiService {
+  final GovernanceController controller;
+
+  GovernanceApiService({
+    required this.controller,
+  });
+
+  Map<String, dynamic> evaluateConcept(
+    int conceptId,
+    Map<String, dynamic> concept,
+  ) {
+    return controller.submitConcept(
+      conceptId,
+      concept,
+    );
+  }
+}
