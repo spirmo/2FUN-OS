a5a7692f9ead1a497a0e6a682cdc8a4daec0a004
spirmo <153791815+spirmo@users.noreply.github.com>
2026-07-24 19:37:54 +0330

    Update role_manager.py

--- platform_core/identity/role_manager.py
+++ platform_core/identity/role_manager.py
@@ -1,1 +1,13 @@
+class RoleManager:
 
+    def __init__(self):
+        self.roles = {}
+
+    def register_role(self, role_name, permissions):
+        self.roles[role_name] = permissions
+
+    def get_permissions(self, role_name):
+        return self.roles.get(role_name, [])
+
+    def has_permission(self, role_name, permission):
+        return permission in self.get_permissions(role_name)
