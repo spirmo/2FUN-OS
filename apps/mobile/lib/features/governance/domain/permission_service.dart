3f484e5e71a785edae6427c0a6dc32363d207337
Saeed PirMoradian <spirmo001@gmail.com>
2026-07-26 00:50:51 +0430

    Sync Flutter governance permissions with TANDIL RoleManager

--- apps/mobile/lib/features/governance/domain/permission_service.dart
+++ apps/mobile/lib/features/governance/domain/permission_service.dart
@@ -7,30 +7,94 @@ class PermissionService {
 
     final permissions = {
 
-      "ADMIN": [
+      "Newbie": [],
+
+      "Active User": [
+        "mission_create",
+      ],
+
+      "Contributor": [
+        "mission_create",
+        "mission_assign",
+      ],
+
+      "Specialist": [
+        "mission_create",
+        "mission_assign",
+        "mission_validate",
+      ],
+
+      "Strategist": [
+        "mission_create",
+        "mission_assign",
+        "mission_validate",
+      ],
+
+      "Veteran": [
+        "mission_create",
+        "mission_assign",
+        "mission_validate",
+      ],
+
+      "Member": [
+        "basic_interaction",
+        "participation",
+      ],
+
+      "Coordinator": [
+        "mission_assign",
+        "community_help",
+      ],
+
+      "Sub-Leader": [
+        "mission_assign",
+        "colony_control",
+      ],
+
+      "Colony Leader": [
+        "colony_control",
+        "user_manage",
+      ],
+
+      "Analyst": [
+        "rule_view",
+      ],
+
+      "Moderator": [
+        "content_review",
+        "mission_validate",
+      ],
+
+      "Validator": [
         "concept_approve",
+        "mission_validate",
+      ],
+
+      "Auditor": [
+        "audit_reports",
+        "rule_view",
+      ],
+
+      "Council Member": [
+        "governance_vote",
+        "rule_view",
+      ],
+
+      "System Overseer": [
+        "all_access",
+        "governance_vote",
         "user_manage",
         "content_review",
         "audit_reports",
       ],
 
-      "GOVERNOR": [
+      "Rule Approver": [
+        "rule_view",
+        "reward_modify",
+        "economy_control",
+        "user_manage",
         "concept_approve",
       ],
-
-      "VALIDATOR": [
-        "concept_approve",
-      ],
-
-      "MODERATOR": [
-        "content_review",
-      ],
-
-      "AUDITOR": [
-        "audit_reports",
-      ],
-
-      "USER": [],
 
     };
 
