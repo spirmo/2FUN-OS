a4f563d6b07328aaf9e50319aadb3d2d405f4596
spirmo <153791815+spirmo@users.noreply.github.com>
2026-07-25 10:07:36 +0330

    Update role_manager.py

--- platform_core/identity/role_manager.py
+++ platform_core/identity/role_manager.py
@@ -1,8 +1,43 @@
 class RoleManager:
 
     def __init__(self):
-        self.roles = {}
+    self.roles = {}
+    self._load_default_roles()
 
+
+    def _load_default_roles(self):
+
+    self.register_role("Newbie", [])
+
+    self.register_role("Active User", [
+        "mission_create",
+    ])
+
+    self.register_role("Contributor", [
+        "mission_create",
+        "mission_assign",
+    ])
+
+    self.register_role("Specialist", [
+        "mission_create",
+        "mission_assign",
+        "mission_validate",
+    ])
+
+    self.register_role("Moderator", [
+        "mission_validate",
+    ])
+
+    self.register_role("Auditor", [
+        "rule_view",
+    ])
+
+    self.register_role("Rule Approver", [
+        "rule_view",
+        "reward_modify",
+        "economy_control",
+        "user_manage",
+    ])
     def register_role(self, role_name, permissions):
         self.roles[role_name] = permissions
 
