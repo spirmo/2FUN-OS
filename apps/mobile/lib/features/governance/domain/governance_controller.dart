de659b182912852e6cc94c68b5f7317a469ed1ff
Saeed PirMoradian <spirmo001@gmail.com>
2026-07-27 06:51:15 +0430

    Test governance permissions

--- apps/mobile/lib/features/governance/domain/governance_controller.dart
+++ apps/mobile/lib/features/governance/domain/governance_controller.dart
@@ -26,29 +26,34 @@ class GovernanceController {
     };
   }
 
-
   List<String> permissionsForRole(String role) {
 
-    final allPermissions = [
+  final allPermissions = [
+    "concept_approve",
+  ];  
 
-      "mission_create",
-      "mission_assign",
-      "mission_validate",
-      "basic_interaction",
-      "participation",
-      "community_help",
-      "colony_control",
-      "user_manage",
-      "rule_view",
-      "content_review",
-      "concept_approve",
-      "audit_reports",
-      "governance_vote",
-      "reward_modify",
-      "economy_control",
-      "all_access",
+ # List<String> permissionsForRole(String role) {
 
-    ];
+   # final allPermissions = [
+
+    #  "mission_create",
+     # "mission_assign",
+     # "mission_validate",
+      #"basic_interaction",
+      #"participation",
+      #"community_help",
+      #"colony_control",
+      #"user_manage",
+      #"rule_view",
+      #"content_review",
+      #"concept_approve",
+      #"audit_reports",
+      #"governance_vote",
+      #"reward_modify",
+      #"economy_control",
+      #"all_access",
+
+    #];
 
 
     return allPermissions
