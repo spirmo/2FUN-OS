f8c41344a84b30b5a2ed326e7415a150027eed9e
Saeed PirMoradian <spirmo001@gmail.com>
2026-07-31 14:54:45 +0430

    Fix sqflite import in database export service

--- apps/mobile/lib/core/database/database_export_service.dart
+++ apps/mobile/lib/core/database/database_export_service.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
-
+import 'package:sqflite/sqflite.dart';
 class DatabaseExportService {
   static Future<String> exportDatabase() async {
     final dbDir = await getDatabasesPath();
