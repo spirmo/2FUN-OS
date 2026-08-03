5f62b27806aeb743d4f00b749e5d5b9dcb1b71fc
Saeed PirMoradian <spirmo001@gmail.com>
2026-08-03 23:39:16 +0430

    Update package name and Android build workflow

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.twofun_app"
+        applicationId = "com.twofun.superapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
