6740c1cfdfbf17afae481a6d40bc1130f724faa6
Saeed PirMoradian <spirmo001@gmail.com>
2026-08-04 08:33:57 +0430

    Update Android namespace to com.twofun.superapp

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.twofun_app"
+    namespace = "com.twofun.superapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
