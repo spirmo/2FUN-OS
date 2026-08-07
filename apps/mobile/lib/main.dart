537431022c27789c9a3782f17c889a4523e578ae
Saeed PirMoradian <spirmo001@gmail.com>
2026-08-07 14:49:22 +0430

    remove diagnostic import from startup

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/widgets.dart';
 
 import 'app/app.dart';
 import 'core/database/database_service.dart';
-import 'core/diagnostic/diagnostic_initializer.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
