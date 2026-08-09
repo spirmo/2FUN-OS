0c8cb021fd5b5cabe9ca789834d3183af617edee
Saeed PirMoradian <spirmo001@gmail.com>
2026-08-09 18:49:17 +0430

    test native Android startup

--- apps/mobile/android/app/src/main/kotlin/com/example/twofun_app/MainActivity.kt
+++ apps/mobile/android/app/src/main/kotlin/com/example/twofun_app/MainActivity.kt
@@ -1,5 +1,18 @@
 package com.example.twofun_app
 
-import io.flutter.embedding.android.FlutterActivity
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
 
-class MainActivity : FlutterActivity()
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val textView = TextView(this)
+        textView.text = "NATIVE ANDROID TEST"
+        textView.textSize = 24f
+
+        setContentView(textView)
+    }
+}
