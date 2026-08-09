414a8ad93fdef62fe2b9fde04306fcadd75f752f
Saeed PirMoradian <spirmo001@gmail.com>
2026-08-09 19:46:47 +0430

    test: minimal flutter activity startup

--- apps/mobile/android/app/src/main/kotlin/com/example/twofun_app/MainActivity.kt
+++ apps/mobile/android/app/src/main/kotlin/com/example/twofun_app/MainActivity.kt
@@ -1,18 +1,5 @@
 package com.example.twofun_app
 
-import android.app.Activity
-import android.os.Bundle
-import android.widget.TextView
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : Activity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val textView = TextView(this)
-        textView.text = "NATIVE ANDROID TEST"
-        textView.textSize = 24f
-
-        setContentView(textView)
-    }
-}
+class MainActivity : FlutterActivity()
