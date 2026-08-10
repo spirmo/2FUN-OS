362147e9410481f5803abdf4180537f0c7e64322
Saeed PirMoradian <spirmo001@gmail.com>
2026-08-10 09:57:51 +0430

    test: restore Android Gradle config before release signing

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -1,22 +1,11 @@
-import java.util.Properties
-import java.io.FileInputStream
-
-val keyProperties = Properties()
-val keyPropertiesFile = rootProject.file("key.properties")
-
-if (keyPropertiesFile.exists()) {
-    keyProperties.load(FileInputStream(keyPropertiesFile))
-}
-
 plugins {
     id("com.android.application")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-
-    namespace = "com.twofun.superapp"
-
+    namespace = "com.example.twofun_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -26,55 +15,31 @@ android {
     }
 
     defaultConfig {
-
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.twofun.superapp"
-
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-    }
-
-    signingConfigs {
-
-        create("release") {
-
-            storeFile = rootProject.file(
-                keyProperties["storeFile"] as String
-            )
-
-            storePassword =
-                keyProperties["storePassword"] as String
-
-            keyAlias =
-                keyProperties["keyAlias"] as String
-
-            keyPassword =
-                keyProperties["keyPassword"] as String
-        }
-    }
+    }   
 
     buildTypes {
-
         release {
-
-            signingConfig =
-                signingConfigs.getByName("release")
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
 kotlin {
-
     compilerOptions {
-
-        jvmTarget =
-            org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
 }
 
 flutter {
-
     source = "../.."
 }
