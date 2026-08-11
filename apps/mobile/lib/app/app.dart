5e1bb23dc907a871553c06f38318ad7c1ac7110e
Saeed PirMoradian <spirmo001@gmail.com>
2026-08-11 07:16:56 +0430

    test: restore main app screen for upgrade validation

--- apps/mobile/lib/app/app.dart
+++ apps/mobile/lib/app/app.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TwoFunApp extends StatelessWidget {
+import '../core/language/language_controller.dart';
+import '../core/routing/app_router.dart';
+import '../core/theme/app_theme.dart';
+import '../features/language/presentation/pages/splash_page.dart';
+
+class TwoFunApp extends StatefulWidget {
   const TwoFunApp({super.key});
 
   @override
+  State<TwoFunApp> createState() => _TwoFunAppState();
+}
+
+class _TwoFunAppState extends State<TwoFunApp> {
+  final LanguageController languageController = LanguageController();
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialization intentionally disabled for startup stability test.
+  }
+
+  @override
+  void dispose() {
+    languageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('2FUN TEST'),
-        ),
-      ),
+    return AnimatedBuilder(
+      animation: languageController,
+      builder: (context, _) {
+        return MaterialApp(
+          title: "2FUN",
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.darkTheme,
+          locale: const Locale("fa"),
+          onGenerateRoute: AppRouter.generate,
+          home: const SplashPage(),
+        );
+      },
     );
   }
 }
